2c04e531bd7a64872aeba2512892b7f850974797
Luan Gazin <luan_gazin@yahoo.com.br>
2016-02-05 00:41:23 +0000

    Bootstrap Material Design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-m1 = Message.create(content: "We're at the beach so you should meet us here! I make a mean sandcastle. :)")
+g1 = Genere.create(name: "Genere One", description: "This is an example of description.")
+g2 = Genere.create(name: "Genere Two", description: "This is another example of description.")
 
-m2 = Message.create(content: "Let's meet there!")
+m1 = Message.create(content: "We're at the beach so you should meet us here! I make a mean sandcastle. :)", genere: g1)
+m2 = Message.create(content: "Let's meet there!", genere: g1)
+m3 = Message.create(content: "Sure!", genere: g2)
 
-m3 = Message.create(content: "Sure!")
